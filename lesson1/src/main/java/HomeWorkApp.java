44fcd917a2f0e1489bd0e70ed6e84bfbefbdeb35
WIN-8HJ9PQKVG3P\user <kulyamin.pavel@gmail.com>
2022-04-11 09:49:06 +0300

    Fix mistake

--- lesson1/src/main/java/HomeWorkApp.java
+++ lesson1/src/main/java/HomeWorkApp.java
@@ -1,20 +1,27 @@
 import java.util.Scanner;
+
 // Первое задание GeekBrains
+// Исправление ошибок указанных Александро Гладковым
+
 public class HomeWorkApp {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         printThreeWords();
-        checkSumSign();
-        printColor();
-        compareNumbers();
+        checkSumSign(scanner);
+        printColor(scanner);
+        compareNumbers(scanner);
+        scanner.close();
     }
 
     public static void printThreeWords() {
-        System.out.println("Orange\nBanana\nApple\n");
+
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
     }
 
-    public static void checkSumSign() {
-        Scanner in = new Scanner(System.in);
+    public static void checkSumSign(Scanner in) {
         System.out.println("Enter a: ");
         int a = in.nextInt();
         System.out.println("Enter b: " );
@@ -27,21 +34,19 @@ public class HomeWorkApp {
         }
     }
 
-    public static void printColor(){
-        Scanner in = new Scanner(System.in);
+    public static void printColor(Scanner in){
         System.out.println("Enter value: ");
         int value = in.nextInt();
         if (value <= 0){
             System.out.println("Красный");
-        } else if (0 < value && value <= 100 ){
+        } else if (value > 0 && value <= 100 ){
             System.out.println("Желтый");
-            } else if (value > 100){
+            } else{
                 System.out.println("Зеленый");
             }
     }
 
-    public static void compareNumbers(){
-        Scanner in = new Scanner(System.in);
+    public static void compareNumbers(Scanner in){
         System.out.println("Enter a: ");
         int a = in.nextInt();
         System.out.println("Enter b: " );
